3571afe0b1222bffdc3d99803a984ef52c9b7460
ORBrier <113340498+ORBrier@users.noreply.github.com>
2023-06-20 09:16:03 +0100

    Update main.py
    draw enemies and player, player moves down and player controls up

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ pygame.init()
 screen = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('My amazing game')
 
-#some variables
-menu = True
+#text variables
+player1_score = 0
+reset_text = ("press SPACE to reset score")
+game_font = pygame.font.SysFont("display", 32)
+outcome_font = pygame.font.SysFont("Calibri", 100)
 
 #colours
 red = (255, 0,0)
@@ -21,50 +24,86 @@ blue = (0, 0, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
 darkBrown = (10, 10, 10)
+menuColour = (100, 70, 60)
 
 # ---Classes--- #
-class enemy(pygame.sprite.Sprite):
-    def __init__(self, color):
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
         super().__init__()
         self.image = pygame.Surface([50, 50])
-        self.image.fill(color)
+        self.image.fill(red)
         self.rect = self.image.get_rect()
+        self.rect.x = random.randint(800,1200)
+        self.rect.y = random.randint(0,800)
 
-class player(pygame.sprite.Sprite):
-    def __init__(self, color):
+
+    def update(self):
+        pass
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
         super().__init__()
         self.image = pygame.Surface([20,30])
-        self.image.fill(color)
+        self.image.fill(blue)
         self.rect = self.image.get_rect()
 
+    def update(self):
+        #pos = pygame.mouse.get_pos()
+        #self.rect.y = pos[1]
+        self.rect.y += 5
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, color, width, height):
+    def __init__(self):
         super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        self.image = pygame.Surface([15, 10])
+        self.image.fill(white)
         self.rect = self.image.get_rect()
 
-    #def update(self):
-     #   self.rect.y -= 10
+    def update(self):
+        self.rect.x += 10
 
-class Object(pygame.sprite.Sprite):
-    def __init__(self, name, width, height, color):
-        super().__init__()
-        self.name = (name)
-        self.image = pygame.Surface([width,height])
-        self.image.fill(color)
-        self.rect = self.image.get_rect()
 
 # --functions-- #
 
+# holding key down
+key_state = {
+}
+def key_held(k):
+    if k in key_state:
+        return key_state[k]
+    else:
+        return False
 
 
 # -----Main code----- #
 
 #start pygame
+pygame.init()
 
-while True:
+#Sprite lists
+# This is a list of every sprite. All blocks and the player block as well.
+all_sprites_list = pygame.sprite.Group()
+
+# List of each block in the game
+enemy_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+
+for x in range(10):
+    enemy = Enemy()
+    enemy_group.add(enemy)
+
+for y in range(1):
+    player = Player()
+    player_group.add(player)
+player.rect.x = 200
+
+
+running = True
+clock = pygame.time.Clock()
+
+
+while running:
     #Handling input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -75,19 +114,37 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
 
-        #moving the player
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                player.rect.x -= 5
+                bullet = Bullet()
+
+                # Set the bullet so it is where the player is
+                bullet.rect.x = player.rect.x
+                bullet.rect.y = player.rect.y
+                # Add the bullet to the lists
+                all_sprites_list.add(bullet)
+                bullet_group.add(bullet)
+
+        #moving the player
+        if event.type == pygame.KEYDOWN:
+            key_state[event.key] = True
+        elif event.type == pygame.KEYUP:
+            key_state[event.key] = False
 
         #menu setup
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_s:
+            if event.key == pygame.K_n:
                 menu = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_m:
+                menu = True
 
-    # --Game logic-- #
-    if menu == True:
-        screen.fill(darkBrown)
+
+        # --Game logic-- #
+    menu = False
+    if menu: # if menu is open
+        screen.fill(menuColour)
+
         #startText = startFont.render("Press S to start", 300, black)
         #screen.blit(startText, (400, 350))
     else:
@@ -97,10 +154,18 @@ while True:
         # Clear the screen
         screen.fill(black)
 
+        if key_held(pygame.K_w):
+            player.rect.y -= 10
+
+        enemy_group.update()
+        player_group.update()
+        bullet_group.update()
+
+        enemy_group.draw(screen)
+        player_group.draw(screen)
+        bullet_group.draw(screen)
         # Draw all the spites
         #all_sprites_list.draw(screen)
-
-        #player_list.draw(screen)
 
 
     #updating the gamme window
