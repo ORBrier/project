966fb9c3bb0eeaedd58113ec50385d2f5a5d24a3
ORBrier <113340498+ORBrier@users.noreply.github.com>
2023-06-20 16:12:10 +0100

    Update main.py
    Organising code/notes
    introduced boss after first wave
    enemies shoot back and kill player
    restart screen made
    Menu screen updated

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ pygame.init()
 screen = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('My amazing game')
 
-#some variables
-menu = False
-player1_score = 0
-reset_text = ("press SPACE to reset score")
+# miscellaneous variables
+menu = True
+restat_screen = False
+score = 0
+dead = False
 game_font = pygame.font.SysFont("display", 32)
-start_font = pygame.font.SysFont("Calibri", 100)
+start_font = pygame.font.SysFont("Calibri", 25)
 
-#colours
+# colours
 red = (255, 0,0)
 green = (0, 255,0)
 blue = (0, 0, 255)
@@ -27,7 +28,7 @@ white = (255, 255, 255)
 darkBrown = (10, 10, 10)
 menuColour = (100, 70, 60)
 
-# ---Classes--- #
+# ---Classes--- # -------------------------------------------------------------------------------------------------------------------------------
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -37,9 +38,26 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.x = random.randint(800,1200)
         self.rect.y = random.randint(0,800)
 
-
     def update(self):
         pass
+
+class Boss(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.Surface([150, 150])
+        self.image.fill(red)
+        self.rect = self.image.get_rect()
+        self.down = True
+
+    def update(self):
+        if self.down:
+            self.rect.y -= 5
+            if self.rect.y <= 10:
+                self.down = False
+        else:
+            self.rect.y += 5
+            if self.rect.y >= 840-150:
+                self.down = True
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -75,7 +93,7 @@ class Shoot(pygame.sprite.Sprite):
         self.rect.x -= 5
 
 
-# --functions-- #
+# --Functions-- # -------------------------------------------------------------------------------------------------------------------------------
 
 # holding key down
 key_state = {
@@ -87,24 +105,30 @@ def key_held(k):
         return False
 
 
-# -----Main code----- #
+# -----Sprites----- # -------------------------------------------------------------------------------------------------------------------------------
 
-#start pygame
+# start pygame
 pygame.init()
 
-#Sprite lists
+# sprite lists
 all_sprites_list = pygame.sprite.Group()
 
 enemy_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 bullet_group = pygame.sprite.Group()
 shoot_group = pygame.sprite.Group()
+boss_group = pygame.sprite.Group()
 
-#number of entities add to sprite groups
-
+# number of entities add to sprite groups
 for x in range(10):
     enemy = Enemy()
     enemy_group.add(enemy)
+
+for z in range(1):
+    boss = Boss()
+    boss_group.add(boss)
+boss.rect.x = 1000
+boss.rect.y = 350
 
 for y in range(1):
     player = Player()
@@ -112,21 +136,29 @@ for y in range(1):
     player_group.add(player)
 player.rect.x = 200
 
-#initialise
+# initialise
 running = True
 clock = pygame.time.Clock()
 
+# ---Main game loop--- # -------------------------------------------------------------------------------------------------------------------------------
+
 while running:
-    #Handling input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
 
-        #Quit game option
+        # quit game
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pygame.quit()
 
+        # restart game
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                menu = True
+                restat_screen = False
+
+        # player firing
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bullet = Bullet()
@@ -138,35 +170,78 @@ while running:
                 # Add the bullet to the lists
                 bullet_group.add(bullet)
 
-        #moving the player
+        # moving the player
         if event.type == pygame.KEYDOWN:
             key_state[event.key] = True
         elif event.type == pygame.KEYUP:
             key_state[event.key] = False
 
-        #menu setup
+        # exit menu
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_n:
                 menu = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_m:
-                menu = True
 
-
-    # --Game logic-- #
+    # --Menu-- # -------------------------------------------------------------------------------------------------------------------------------
     if menu: # if menu is open
         screen.fill(menuColour)
 
-        startText = start_font.render("Press n to start", 300, white)
-        screen.blit(startText, (400, 350))
+        button_width = 220
+        button_height = 50
+        button_x = (display_width - button_width) // 2
+        start_button_y = (display_height - button_height) // 2 - 50
+        quit_button_y = (display_height - button_height) // 2 + 50
+
+        # start button
+        start_button_rect = pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(button_x, start_button_y, button_width, button_height))
+        start_font = pygame.font.Font(None, 36)
+        start_text = start_font.render("Press N to start", True, (255, 255, 255))
+        start_text_rect = start_text.get_rect(center=start_button_rect.center)
+        screen.blit(start_text, start_text_rect)
+
+        # exit button
+        quit_button_rect = pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(button_x, quit_button_y, button_width, button_height))
+        quit_font = pygame.font.Font(None, 36)
+        quit_text = quit_font.render("Press ESC to exit", True, (255, 255, 255))
+        quit_text_rect = quit_text.get_rect(center=quit_button_rect.center)
+        screen.blit(quit_text, quit_text_rect)
+
+    # --Restart screen-- # -------------------------------------------------------------------------------------------------------------------------------
+    elif restat_screen == True:
+
+            # death screen (with img in files)
+            you_died_image = pygame.image.load("you_died.jpg")
+            x = (display_width - you_died_image.get_width()) // 2
+            y = (display_height - you_died_image.get_height()) // 2
+            screen.blit(you_died_image, (x, y))
+
+            # restart game text
+            startText = start_font.render("Press r to restart", 300, white)
+            screen.blit(startText, (600, 550))
+
+    # --Game screen-- # -------------------------------------------------------------------------------------------------------------------------------
     else:
-        # Clear the screen
+
+        # backround design
         screen.fill(black)
 
+        # score text
+        scoreTxt = game_font.render(f"Score: {score}", False, white)
+        screen.blit(scoreTxt, (10, 10))
+
+        # hold to rise up screen
         if key_held(pygame.K_w) and player.rect.y > 0:
             player.rect.y -= 10
 
-        # Calculate mechanics for each bullet
+        # enemies shooting
+        for enemy in enemy_group:
+            chance = random.randint(0, 10000)
+            if (chance % 333) == 0:
+                shoot = Shoot()
+                shoot.rect.x = enemy.rect.x
+                shoot.rect.y = enemy.rect.y
+                shoot_group.add(shoot)
+
+        # calculate mechanics for each bullet
         for bullet in bullet_group:
 
             # See if it hit a block
@@ -176,27 +251,45 @@ while running:
             for enemy in block_hit_list:
                 bullet_group.remove(bullet)
                 all_sprites_list.remove(bullet)
-                #score += 1
+                score += 1
 
             # Remove the bullet if it flies up off the screen
             if bullet.rect.x < -10 or bullet.rect.x > 1400:
                 bullet_group.remove(bullet)
                 all_sprites_list.remove(bullet)
 
-        #sprites
+        # calculate mechanics for player dead and shooting from enemy (same code as bullets and enemies)
+        for shoot in shoot_group:
+            health_hit_list = pygame.sprite.spritecollide(shoot, player_group, True)
+            for player in health_hit_list:
+                shoot_group.remove(player)
+                all_sprites_list.remove(shoot)
+                dead = True
+            if shoot.rect.x < -10 or shoot.rect.x > 1400:
+                shoot_group.remove(shoot)
+                all_sprites_list.remove(shoot)
+
+        # condition in game end
+        if dead:
+            restat_screen = True
+
+
+        # sprites update
         enemy_group.update()
         player_group.update()
         bullet_group.update()
+        shoot_group.update()
+        boss_group.update()
 
+        # sprites draw
         enemy_group.draw(screen)
         player_group.draw(screen)
         bullet_group.draw(screen)
         shoot_group.draw(screen)
+        if score == 10: # after first wave dies
+            boss_group.draw(screen)
 
-        #draw a background
-
-
-    #updating the gamme window
+    # updating the gamme window
     pygame.display.flip()
     clock.tick(75)
 
